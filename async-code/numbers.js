7ff81e6f67606189052dcb492e93a0dcd601260b
Billy Russell <billy.hrussell@gmail.com>
2022-08-15 17:36:17 -0700

    adding files

--- async-code/numbers.js
+++ async-code/numbers.js
@@ -5,32 +5,49 @@ const JSON = "?json";
 
 let $oneNum = $("#one-num");
 let $manyNums = $("#multiple-nums");
+const $oneNumMany = $("#one-num-many-rq");
+
+const favNum = 5;
 
 async function makeRequest(){
-  const favNum = 5;
 
-  try{
-    const fact = await axios.get(`${BASE_URL}${favNum}${JSON}`);
+
+  // try{
+    const fact = await axios.get(`${BASE_URL}${favNum}?json`);
 
     //let parsed = JSON.stringify(fact);
     // console.log(parsed);
-    console.log(fact.data);
-    // $oneNum.text(fact.text);
-  }catch(err){
+    console.log("fact.data", fact.data);
+    console.log("fact.data.text", fact.data.text);
+
+    $oneNum.text("fact.data.text",fact.data.text);
+  // }catch(err){
     console.log("You got an error");
-  }
+  // }
 }
 
 async function makeMultipleRequest(){
   const favNums = "2,3,4,5,6";
 
-  try{
     fact = await axios({url: `${BASE_URL}${favNums}${JSON}`});
-  }catch(err){
-    console.log("You got an error");
-  }
+    $manyNums.text(fact)
+
 }
 
+async function oneNumFourRq(){
+
+  const fact1 = axios({url: `${BASE_URL}${favNum}${JSON}`});
+  const fact2 = axios({url: `${BASE_URL}${favNum}${JSON}`});
+  const fact3 = axios({url: `${BASE_URL}${favNum}${JSON}`});
+  const fact4 = axios({url: `${BASE_URL}${favNum}${JSON}`});
+
+  let results = await Promise.allSettled([fact1,fact2,fact3,fact4]);
+  let facts = results.map(res => res.data.text);
+  $oneNumMany.text(facts);
+
+}
 
 makeRequest();
+makeMultipleRequest();
+oneNumFourRq();
 
